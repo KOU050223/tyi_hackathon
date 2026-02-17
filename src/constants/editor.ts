1e814e45d08f3a34e5e13d8a7286d6ec26c7f6e6
KOU050223 <kouhei20050223@icloud.com>
2026-02-17 14:54:26 +0900

    refactor: 型定義と定数を分離してsrc/constants/に集約
    src/types/に混在していた定数をsrc/constants/以下に移動し、
    コンポーネントやutilにハードコードされた定数も集約する。

---
+++ src/constants/editor.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_COLOR = "#E66CBC";
+
+export const PRESET_COLORS = [
+  "#E66CBC",
+  "#FFFF00",
+  "#FF0000",
+  "#4444FF",
+  "#FF6600",
+  "#FF00FF",
+  "#00FFFF",
+  "#FFAAAA",
+  "#FFAA00",
+  "#FFFFFF",
+] as const;
